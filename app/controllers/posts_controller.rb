af4000ac70b8d043f6d226865ed6f6a83e344b50
ksltsn <romakislitsin89@gmail.com>
2018-08-28 04:45:03 +0400

    index action for posts with pagination and query headers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,15 @@ class PostsController < ApplicationController
   # Use Knock to make sure the current_user is authenticated before completing request.
   before_action :authenticate_user
 
+  # Method to show all posts ordered by published date.
+  def index
+    per_page = params[:per_page] ? params[:per_page] : Post::PER_PAGE
+    @posts = Post.by_published_date.paginate(page: params[:page], per_page: per_page)
+    # Set count of posts and count of pages to query headers
+    add_headers
+    render json: @posts
+  end
+
   # Method to create a new users post using the safe params we setup.
   def create
     @post = current_user.posts.build(post_params)
@@ -32,4 +41,16 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body, :published_at, :author_id)
   end
 
+  def add_headers
+    response.set_header("posts_count", posts_count)
+    response.set_header("pages_count", pages_count)
+  end
+
+  def posts_count
+    Post.count.to_s
+  end
+
+  def pages_count
+    @posts.total_pages
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   validates_presence_of     :title
   validates_presence_of     :body
 
+  scope :by_published_date, -> { order(published_at: :desc) }
+
+  PER_PAGE = 10
+
   def set_time(time)
     self.published_at = time
   end
