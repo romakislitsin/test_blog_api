d2db5d77f239e417fc5c0bea600469e06a0b1c1a
ksltsn <romakislitsin89@gmail.com>
2018-09-01 09:25:31 +0400

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+  User.create(
+      email: "user#{i}@example.com",
+      username: Faker::GameOfThrones.character,
+      password: "password_#{i}",
+      password_confirmation: "password_#{i}"
+  )
+end
+
+100.times do
+  Post.create(
+      title: Faker::StarWars.wookie_sentence,
+      body: Faker::StarWars.quote,
+      published_at: Time.now - rand(5).days,
+      author_id: User.all.sample.id
+  )
+end
+
+100.times do
+  Comment.create(
+      body: Faker::RickAndMorty.quote,
+      published_at: Time.now - rand(5).days,
+      author_id: User.all.sample.id,
+      post_id: Post.all.sample.id
+  )
+end
